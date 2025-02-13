62b32e1be3c2af1f1fe9a796d9e4dedab8df7fbe
Noxin D Victus <jvcalma24@gmail.com>
2025-02-13 21:01:50 +0700

    feat: create AuthService

---
+++ Api/Services/AuthService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace TechBodiaApi.Api
+{
+    public class AuthService
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+        }
+    }
+}
