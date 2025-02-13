16ca67a2542a39bdd61074dce210daf2af680f83
Noxin D Victus <jvcalma24@gmail.com>
2025-02-13 21:01:15 +0700

    feat: create TokenAuthService

---
+++ Api/Services/TokenAuthService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace TechBodiaApi.Api.Services
+{
+    public class TokenAuthService
+    {
+        public void ConfigureServices(IServiceCollection services, byte[] key)
+        {
+            // Register services
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false; // Set to true in production
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                    };
+                });
+        }
+    }
+}
