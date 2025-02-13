15297a86b073399770bb7e0284d3b77e768cc4bd
Noxin D Victus <jvcalma24@gmail.com>
2025-02-13 21:01:38 +0700

    feat: create CorsPolicyService

---
+++ Api/Services/CorsPolicyService.cs
@@ -0,0 +1,23 @@
+﻿namespace TechBodiaApi.Api.Extenstions
+{
+    public class CorsPolicyService
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "AllowFrontend",
+                    policy =>
+                    {
+                        policy
+                            .WithOrigins("https://localhost:3000", "https://noxdinvictus.github.io")
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                            .AllowCredentials();
+                    }
+                );
+            });
+        }
+    }
+}
