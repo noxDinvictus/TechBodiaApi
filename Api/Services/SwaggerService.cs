80b1cc0c5b6f5f8e6f031dab9d17b6c9a76298ef
Noxin D Victus <jvcalma24@gmail.com>
2025-02-13 21:01:29 +0700

    feat: create SwaggerService

---
+++ Api/Services/SwaggerService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace TechBodiaApi.Api.Extenstions
+{
+    public class SwaggerService
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TechBodia API", Version = "v1" });
+
+                c.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "Enter 'Bearer {token}'",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.Http,
+                        Scheme = "Bearer",
+                    }
+                );
+
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer",
+                                },
+                            },
+                            new List<string>()
+                        },
+                    }
+                );
+            });
+        }
+    }
+}
