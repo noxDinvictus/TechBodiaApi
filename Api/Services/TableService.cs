f4695fc97d0a723458661ace6eb4650a986f1ca5
Noxin D Victus <jvcalma24@gmail.com>
2025-02-13 21:01:23 +0700

    feat: create TablesService

---
+++ Api/Services/TableService.cs
@@ -0,0 +1,15 @@
+﻿using TechBodiaApi.Services.Implementations;
+using TechBodiaApi.Services.Interfaces;
+
+namespace TechBodiaApi.Api
+{
+    public class TablesService
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register services
+            services.AddScoped<IUserServices, UserServices>();
+            services.AddScoped<INoteServices, NoteServices>();
+        }
+    }
+}
