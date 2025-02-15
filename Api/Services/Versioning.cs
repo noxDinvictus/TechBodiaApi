1432c4b596d581b6d98834b0bff72c32e7bbc16e
Noxin D Victus <jvcalma24@gmail.com>
2025-02-15 15:31:00 +0700

    feat: create Versioning, to handle different versions

---
+++ Api/Services/Versioning.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+
+namespace TechBodiaApi.Api.Services
+{
+    public class Versioning
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            });
+        }
+    }
+}
