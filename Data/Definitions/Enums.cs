df5e07f2a3ad52012ea84602ea6c2daaf1fb14a6
Noxin D Victus <jvcalma24@gmail.com>
2025-02-15 16:36:22 +0700

    feat: create enum Roles, admin and user

---
+++ Data/Definitions/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace TechBodiaApi.Data.Definitions
+{
+    public enum Roles
+    {
+        [EnumMember(Value = "User")]
+        User = 1,
+
+        [EnumMember(Value = "Admin")]
+        Admin = 9,
+    }
+}
