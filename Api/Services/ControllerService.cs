9b9a6895bd7c402401a2b548344ba367cfc22c29
Noxin D Victus <jvcalma24@gmail.com>
2025-02-22 01:08:20 +0700

    feat: create ControllerService, this will help to generate enum as strings, will ignore objects from Newtonsoft.Json, if it has [JsonIgnore]

---
+++ Api/Services/ControllerService.cs
@@ -0,0 +1,37 @@
+﻿namespace TechBodiaApi.Api.Services
+{
+    public class ControllerService
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            /**
+             * Allows enum to be generated as string
+             * Example
+             *
+             * [EnumMember(Value = "User")]
+             * User = 1,
+             *
+             * When generated in the front end it will be
+             *
+             * User = 'User',
+             *
+             * NOTE: for objects you want to be ignored, use "using Newtonsoft.Json"; note "using System.Text.Json.Serialization;"
+             */
+            services
+                .AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+                        .Json
+                        .ReferenceLoopHandling
+                        .Ignore;
+                    options.SerializerSettings.Converters.Add(
+                        new Newtonsoft.Json.Converters.StringEnumConverter()
+                    );
+                });
+
+            // Add Swagger support for Newtonsoft.Json
+            services.AddSwaggerGenNewtonsoftSupport(); // Explicitly add this for EnumMember to work
+        }
+    }
+}
