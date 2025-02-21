f566fea441263e0abbf6c8b62992869c3609beda
Noxin D Victus <jvcalma24@gmail.com>
2025-02-21 09:26:56 +0700

    feat: create BaseClass

---
+++ Data/Models/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechBodiaApi.Data.Models
+{
+    public class BaseClass
+    {
+        public DateTime? CreatedAt { get; set; }
+
+        [ForeignKey(nameof(User))]
+        [Required]
+        public Guid CreatedByUserId { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public Guid? UpdatedByUserId { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
